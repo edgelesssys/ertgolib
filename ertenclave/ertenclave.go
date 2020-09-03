0cb0eccf204a7f36ced51f1a583c604311bf1c28
Thomas Tendyck <tt@edgeless.systems>
2020-09-03 16:11:11 +0200

    seal

--- ertenclave/ertenclave.go
+++ ertenclave/ertenclave.go
@@ -1,4 +1,4 @@
-// Package ertenclave provides functionality for Go enclaves. Currently, this is limited to remote attestation.
+// Package ertenclave provides functionality for Go enclaves like remote attestation and sealing.
 package ertenclave
 
 // #cgo LDFLAGS: -Wl,-unresolved-symbols=ignore-in-object-files
@@ -65,6 +65,46 @@ func VerifyRemoteReport(reportBytes []byte) (ert.Report, error) {
 		SignerID:        C.GoBytes(unsafe.Pointer(&report.identity.signer_id[0]), C.OE_SIGNER_ID_SIZE),
 		ProductID:       C.GoBytes(unsafe.Pointer(&report.identity.product_id[0]), C.OE_PRODUCT_ID_SIZE),
 	}, nil
+}
+
+// GetUniqueSealKey gets a key derived from a measurement of the enclave.
+//
+// keyInfo can be used to retrieve the same key later, on a newer security version.
+func GetUniqueSealKey() (key, keyInfo []byte, err error) {
+	return getSealKeyByPolicy(C.OE_SEAL_POLICY_UNIQUE)
+}
+
+// GetProductSealKey gets a key derived from the signer and product id of the enclave.
+//
+// keyInfo can be used to retrieve the same key later, on a newer security version.
+func GetProductSealKey() (key, keyInfo []byte, err error) {
+	return getSealKeyByPolicy(C.OE_SEAL_POLICY_PRODUCT)
+}
+
+// GetSealKey gets a key from the enclave platform using existing key information.
+func GetSealKey(keyInfo []byte) ([]byte, error) {
+	var keyBuffer *C.uint8_t
+	var keySize C.size_t
+	if res := C.oe_get_seal_key_v2((*C.uint8_t)(&keyInfo[0]), C.size_t(len(keyInfo)), &keyBuffer, &keySize); res != C.OE_OK {
+		return nil, oeError(res)
+	}
+
+	key := C.GoBytes(unsafe.Pointer(keyBuffer), C.int(keySize))
+	C.oe_free_seal_key(keyBuffer, nil)
+	return key, nil
+}
+
+func getSealKeyByPolicy(sealPolicy C.oe_seal_policy_t) (key, keyInfo []byte, err error) {
+	var keyBuffer, keyInfoBuffer *C.uint8_t
+	var keySize, keyInfoSize C.size_t
+	if res := C.oe_get_seal_key_by_policy_v2(sealPolicy, &keyBuffer, &keySize, &keyInfoBuffer, &keyInfoSize); res != C.OE_OK {
+		return nil, nil, oeError(res)
+	}
+
+	key = C.GoBytes(unsafe.Pointer(keyBuffer), C.int(keySize))
+	keyInfo = C.GoBytes(unsafe.Pointer(keyInfoBuffer), C.int(keyInfoSize))
+	C.oe_free_seal_key(keyBuffer, keyInfoBuffer)
+	return
 }
 
 func oeError(res C.oe_result_t) error {
